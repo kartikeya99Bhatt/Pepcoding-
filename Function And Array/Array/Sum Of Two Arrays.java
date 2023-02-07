9ce51fda7e2914ee7bdc9907c6eb2eb9efbd6050
Kartikeya Bhatt <bhattkartikeya99@gmail.com>
2023-02-07 20:49:27 +0530

    Update Sum Of Two Arrays.java

--- Function And Array/Array/Sum Of Two Arrays.java
+++ Function And Array/Array/Sum Of Two Arrays.java
@@ -18,7 +18,7 @@ public static void main(String[] args) throws Exception {
     
     int c=0;int i= n1-1;int j= n2-1;int temp=1;
     int ans=0;
-    while(i>=0 || c>0 || j>=0)
+    while(j>=0||c>0||i>=0)
     {
        int ae=(i>=0)?a[i]:0;
        int be=(j>=0)?b[j]:0;
@@ -29,11 +29,12 @@ public static void main(String[] args) throws Exception {
        i--;
        j--;
        temp=temp*10;
+      
     }
     // printing the ans
-    StringBuilder sb=new StringBuilder(ans+"");
-    for( i=0;i<sb.length();i++)
-    System.out.println(sb.charAt(i));
+   StringBuilder sb=new StringBuilder(ans+"");
+   for( i=0;i<sb.length();i++)
+   System.out.println(sb.charAt(i));
     
  }
 
