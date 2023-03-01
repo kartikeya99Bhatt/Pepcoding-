8a0baeb9002cadfbba70756a11512b3d9b8ab253
Kartikeya Bhatt <bhattkartikeya99@gmail.com>
2023-03-01 09:16:14 +0530

    Create Remove Node From Bst.java

---
+++ BST/Remove Node From Bst.java
@@ -0,0 +1,38 @@
+ public static Node remove(Node node, int data) {
+    // write your code here
+    if(node==null)
+    {
+        return null;
+    }
+    if(node.data==data)
+    {
+        if(node.left==null && node.right==null)
+        {
+            return null;
+        }
+        else if(node.left!=null && node.right!=null)
+        {
+            int newdata=getLeftMax(node.left);
+            node.data=newdata;
+            node.left=remove(node.left,node.data);
+        }
+        else if(node.left==null)
+        {
+            return (node.right);
+        }
+        else if(node.right==null)
+        {
+            return (node.left);
+        }
+    }
+    
+    if(node.data>data)
+    {
+        node.left=remove(node.left,data);
+    }
+    else if(node.data<data)
+    {
+        node.right=remove(node.right,data);
+    }
+     return node;
+  }
