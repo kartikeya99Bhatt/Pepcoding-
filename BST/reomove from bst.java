07a9ae9e4c93f8e40aa18198f819a2a5054f114f
Kartikeya Bhatt <bhattkartikeya99@gmail.com>
2023-03-01 09:33:57 +0530

    Create reomove from bst.java

---
+++ BST/reomove from bst.java
@@ -0,0 +1,47 @@
+  public static int getLeftMax(Node node)
+  {
+      while(node.right!=null)
+      {
+          node=node.right;
+      }
+      return node.data;
+  }
+  public static Node remove(Node node, int data) {
+    // write your code here
+    if(node==null)
+    {
+        return null;
+    }
+    System.out.println(node.data);
+    if(node.data==data)
+    {
+        if(node.left==null && node.right==null)
+        {
+            return null;
+        }
+        else if(node.left!=null && node.right!=null)
+        {
+            int newdata=getLeftMax(node.left);
+            node.data=newdata;
+            node.left=remove(node.left,node.data);
+        }
+        else if(node.left==null)
+        {
+            return (node.right);
+        }
+        else if(node.right==null)
+        {
+            return (node.left);
+        }
+    }
+    
+    if(node.data>data)
+    {
+        node.left=remove(node.left,data);
+    }
+    else if(node.data<data)
+    {
+        node.right=remove(node.right,data);
+    }
+     return node;
+  }
